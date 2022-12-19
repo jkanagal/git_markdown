bac504d070dada4de6b262e7b4ad3506ac4d4043
Jana K <31860875+jkanagal@users.noreply.github.com>
2022-12-19 07:25:17 -0500

    iss

---
+++ async/networking/net_example/iss_spotter/index.js
@@ -0,0 +1,48 @@
+// index.js
+
+// The code below is temporary and can be commented out.
+const { fetchMyIP, fetchCoordsByIP, nextISSTimesForMyLocation, fetchISSFlyOverTimes } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:', ip);
+});
+
+fetchCoordsByIP('173.35.139.120', (error, data) => {
+  if (error) {
+    console.log('error: ',error);
+  }
+  console.log('data: ',data);
+});
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
