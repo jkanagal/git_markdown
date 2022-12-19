4799bd3b91cfec64dc51d792e8b1d7ecfa3f4b2a
Jana K <31860875+jkanagal@users.noreply.github.com>
2022-12-18 23:37:25 -0500

    timer

--- async/timer/timer1.js
+++ async/timer/timer1.js
@@ -2,9 +2,23 @@
 
 const input = process.argv.slice(2);
 
-for (let i = 0; i < input.length; i++) {
-  setTimeout(() => {
-    process.stdout.write(`'\x07' : dings at ${input[i]} \n`);
-  }, input[i]);
+const inputcheck = function () {
+  let result = true;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] < 0 || (input[i].isNaN)) {
+      return result = false;
+    }
+  } return result;
+};
+
+if (input.length !== 0 && inputcheck() === true) {
+  for (let i = 0; i < input.length; i++) {
+    setTimeout(() => {
+      process.stdout.write(`'\x07' : dings at ${input[i]} second \n`);
+    }, input[i]);
+  }
+} else {
+  return process.stdout.write('not valid');
 }
+
 
