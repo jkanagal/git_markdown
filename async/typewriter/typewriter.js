a3100e613b5cda33fff1675de0f9c708e708262a
Jana K <31860875+jkanagal@users.noreply.github.com>
2022-12-18 22:15:00 -0500

    typewriter

---
+++ async/typewriter/typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+  }, i * 50);
+}
+
+process.stdout.write('\n');
+
